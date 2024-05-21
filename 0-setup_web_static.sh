e106e5ace1f684d4f1235fbe4f142357b3cc60f0
CObitimah <obitimahcephas@gmail.com>
2024-05-21 05:26:32 -0400

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,55 +1,50 @@
 #!/usr/bin/env bash
-# This script sets up web servers for the deployment of web_static
+# update packages abd install nginx and create directories
+sudo apt-get -y update
+sudo apt-get install -y nginx
 
-#Install Nginx if not already installed
-if ! dpkg -l | grep -q nginx; then
-	sudo apt-get update
-	sudo apt-get install -y nginx
-fi
-
-#Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
+# Creating files and directories
+sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared/
 
-#Create a fake HTML file
+# create html file
 sudo tee /data/web_static/releases/test/index.html > /dev/null << EOF
 <html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>
 EOF
 
-#Create a symbolic link, force remove if it already exists
+# Check if the symbolic link already exists and delete it
 rm -rf /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of /data/ folder
+# giving owndership of data directory to user and group ubuntu
 sudo chown -R ubuntu:ubuntu /data/
 
-# The Nginx configuration update
 sudo tee /etc/nginx/sites-available/default >/dev/null <<EOF
 server {
-	listen 80;
-	listen [::]:80 default_server;
+        listen 80;
+        listen [::]:80 default_server;
+        server_name _;
+        add_header X-Served-By "${HOSTNAME}";
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+        location @404 {
+                 return 404 "Ceci n'est pas une page.";
+        }
 
-	server_name _;
-	add_header X-Served-By "${HOSTNAME}";
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-	location @404 {
-		return "Ceci n'est pas une page.";
-	}
+        location /hbnb_static {
+                 alias /data/web_static/current/;
+        }
 
-	location /hbnb_static/current/ {
-		alias /data/web_static/current/;
-	}
-	
-	root /var/www/html;
-	error_page 404 = @404;
+
+        root /var/www/html;
+        error_page 404 = @404;
 }
 EOF
 

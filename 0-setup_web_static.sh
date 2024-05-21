14be36df3eadd99f063f0f129c5d7afd0fd5d547
CObitimah <obitimahcephas@gmail.com>
2024-05-21 05:22:18 -0400

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,43 +12,45 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
 #Create a fake HTML file
-echo "<html>
-<head>
-</head>
-<body>
-	Holberton School
-</body>
-<html>" | sudo tee /data/web_static/releases/test/index.html
+sudo tee /data/web_static/releases/test/index.html > /dev/null << EOF
+<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+</html>
+EOF
+
 #Create a symbolic link, force remove if it already exists
+rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of /data/ folder
 sudo chown -R ubuntu:ubuntu /data/
 
 # The Nginx configuration update
-config="server {
-	listen 80 default_server;
+sudo tee /etc/nginx/sites-available/default >/dev/null <<EOF
+server {
+	listen 80;
 	listen [::]:80 default_server;
 
-	root /var/www/html;
-	index.html index.htm index.nginx-debian.html;
-
 	server_name _;
-
-	location / {
-		try_files \$uri \$uri/ =404;
+	add_header X-Served-By "${HOSTNAME}";
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+	location @404 {
+		return "Ceci n'est pas une page.";
 	}
 
-	location /hbnb_static {
-		alias /data/web_static/current;
-		index index.html index.htm;
+	location /hbnb_static/current/ {
+		alias /data/web_static/current/;
 	}
-}"
+	
+	root /var/www/html;
+	error_page 404 = @404;
+}
+EOF
 
-echo "$config" | sudo tee /etc/nginx/sites-available/default
-
-# Restart Nginx
 sudo service nginx restart
-
-# Successful exit
-exit 0

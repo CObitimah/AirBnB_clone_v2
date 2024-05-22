03ac4856d7d337bb65d4f26141b839e33a26b4ba
CObitimah <obitimahcephas@gmail.com>
2024-05-22 04:30:45 -0400

    Deploy archieve

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 Fabric script that generates a .tgz archive from the
 contents of the web_static folder of your AirBnB Clone repo."""
 import os
-from fabric.api import *
+from fabric.api import local, run, put, env
 from datetime import datetime
 
 env.hosts = ['54.237.118.67', '52.91.146.16']
@@ -12,29 +12,29 @@ env.key_filename = '/root/.ssh/id_rsa'
 # '~/.ssh/id_rsa'
 
 
-@task
 def do_pack():
     """Function to generate a .tgz archive"""
     # Creating the folder versions
-    local('sudo mkdir -p versions')
+    local('mkdir -p versions')
 
     # Getting the current time
     time_stamp = datetime.now()
     str_time = time_stamp.strftime("%Y%m%d%H%M%S")
+    archieve_path = f'versions/web_static_{str_time}.tgz'
 
-    local(f'sudo tar -cvzf versions/web_static_{str_time}.tgz web_static')
+    result = local(f'tar -cvzf {archieve_path} web_static')
 
-    file_path = f"versions/web_static_{str_time}.tgz"
-    file_size = os.path.getsize(file_path)
+    if result.failed:
+        return None
 
-    if os.path.exists(file_path):
-        print(f"web_static packed: {file_path} -> {file_size}Bytes")
+    file_size = os.path.getsize(archieve_path)
+    if os.path.exists(archieve_path):
+        print(f"web_static packed: {archieve_path} -> {file_size}Bytes")
+        return archieve_path
     else:
         return None
 
 
-# Path: 2-do_deploy_web_static.py
-@task
 def do_deploy(archive_path):
     """
     Fabric script (based on the file 1-pack_web_static.py
@@ -51,20 +51,22 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
 
         # Create the directory to uncompress the file
-        run(f"sudo mkdir -p /data/web_static/releases/{file_name}")
+        run(f"mkdir -p /data/web_static/releases/{file_name}")
         path = f"/data/web_static/releases/{file_name}"
+        
         # Uncompress the file into the created directory
-        run(f"sudo tar -xzf /tmp/{archive_name} -C {path}")
+        run(f"tar -xzf /tmp/{archive_name} -C {path}")
 
         # Delete the archive from the web server
-        run(f"sudo rm /tmp/{archive_name}")
+        run(f"rm /tmp/{archive_name}")
 
         # Delete the symbolic link
-        run("sudo rm -rf /data/web_static/current")
+        run("rm -rf /data/web_static/current")
 
         path_s = f"/data/web_static/current"
+
         # Create a new symbolic link
-        run(f"sudo ln -sf /data/web_static/releases/{file_name}/ {path_s}")
+        run(f"ln -sf /data/web_static/releases/{file_name}/ {path_s}")
 
         # Finish the deployment
         print("New version deployed!")

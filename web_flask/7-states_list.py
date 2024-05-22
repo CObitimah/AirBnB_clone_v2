5a64d9dadda3390e8c9e41a98e8b20f15669e232
CObitimah <obitimahcephas@gmail.com>
2024-05-22 05:40:50 -0400

    Web Flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Start link class to table in database."""
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+states = storage.all(State)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page."""
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

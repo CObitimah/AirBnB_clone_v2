59f50f9664b1efcd55f45a27e1a784755953a988
CObitimah <obitimahcephas@gmail.com>
2024-07-04 11:26:05 -0400

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,88 +1,62 @@
-#!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the
-contents of the web_static folder of your AirBnB Clone repo."""
-import os
-from fabric.api import *
-from datetime import datetime
+from fabric.api import env, local, put, run
+from os.path import exists
+import time
 
-env.hosts = ['54.237.118.67', '52.91.146.16']
-env.username = 'ubuntu'
-env.key_filename = '/root/.ssh/id_rsa'
-# '~/.ssh/id_rsa'
+env.hosts = ['<IP web-01>', '<IP web-02>']
+env.user = 'ubuntu'
+env.key_filename = 'my_ssh_private_key'
 
-
-@task
 def do_pack():
-    """Function to generate a .tgz archive"""
-    # Creating the folder versions
-    local('sudo mkdir -p versions')
-
-    # Getting the current time
-    time_now = datetime.now()
-    str_time = time_now.strftime("%Y%m%d%H%M%S")
-
-    local(f'sudo tar -cvzf versions/web_static_{str_time}.tgz web_static')
-
-    file_path = f"versions/web_static_{str_time}.tgz"
-    file_size = os.path.getsize(file_path)
-
-    if os.path.exists(file_path):
-        print(f"web_static packed: {file_path} -> {file_size}Bytes")
-    else:
+    """Generate a .tgz archive from the contents of the web_static folder."""
+    try:
+        local("mkdir -p versions")
+        timestamp = time.strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
         return None
 
-
-# Path: 2-do_deploy_web_static.py
-@task
 def do_deploy(archive_path):
-    """
-    Fabric script (based on the file 1-pack_web_static.py
-    that distributes an archive to your web servers, using
-    the function do_deploy.
-    """
-    if not os.path.exists(archive_path):
+    """Distribute an archive to the web servers."""
+    if not exists(archive_path):
         return False
     try:
-        archive_name = archive_path.split("/")[-1]
-        file_name = archive_name.split(".")[0]
-
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-
-        # Create the directory to uncompress the file
-        run(f"sudo mkdir -p /data/web_static/releases/{file_name}")
-        path = f"/data/web_static/releases/{file_name}"
-        # Uncompress the file into the created directory
-        run(f"sudo tar -xzf /tmp/{archive_name} -C {path}")
-
-        # Delete the archive from the web server
-        run(f"sudo rm /tmp/{archive_name}")
-
-        # Delete the symbolic link
-        run("sudo rm -rf /data/web_static/current")
-
-        path_s = f"/data/web_static/current"
+        
+        # Extract the filename without extension
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        
+        # Create the release directory
+        release_dir = "/data/web_static/releases/{}".format(no_ext)
+        run("mkdir -p {}".format(release_dir))
+        
+        # Uncompress the archive to the folder
+        run("tar -xzf /tmp/{} -C {}".format(filename, release_dir))
+        
+        # Remove the archive from the web server
+        run("rm /tmp/{}".format(filename))
+        
+        # Move the content from web_static to the release folder
+        run("mv {}/web_static/* {}".format(release_dir, release_dir))
+        
+        # Remove the now empty web_static folder
+        run("rm -rf {}/web_static".format(release_dir))
+        
+        # Delete the current symbolic link
+        run("rm -rf /data/web_static/current")
+        
         # Create a new symbolic link
-        run(f"sudo ln -sf /data/web_static/releases/{file_name}/ {path_s}")
-
-        # Finish the deployment
-        print("New version deployed!")
-    except Exception as e:
-        print(e)
-        print("Deployment failed!")
+        run("ln -s {} /data/web_static/current".format(release_dir))
+        
+        return True
+    except:
         return False
-    # Return True if the deployment was successful
-    return True
 
-
-@task
 def deploy():
-    """
-    Fabric script (based on the file 2-do_deploy_web_static.py)
-    that creates and distributes an archive to your web servers,
-    using the function deploy.
-    """
+    """Create and distribute an archive to the web servers."""
     archive_path = do_pack()
     if archive_path is None:
         return False
